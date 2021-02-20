e53f2711db1e00e3fe66e5bfa53798ba11fddeba
Tomi1989 <tzivkovic89@gmail.com>
2021-02-20 15:54:45 +0100

    upload project

---
+++ src/main/java/com/zadatak/ht/HtApplication.java
@@ -0,0 +1,13 @@
+package com.zadatak.ht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtApplication.class, args);
+    }
+
+}
